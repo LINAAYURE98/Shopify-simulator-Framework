391c97f402f8cec3cf79957135a24b37c1b04ada
LinaAyure <linaayure98@gmail.com>
2025-02-01 14:53:21 -0500

    feat(top-bar): add marquee behavior for scrolling text

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('Gradiweb is online!!!');
+document.addEventListener("DOMContentLoaded", function () {
+    const marqueeText = document.querySelector("#marquee-text .marquee-content");
+    const text = document.querySelector("#marquee-text").getAttribute("data-text");
+
+    if (text) {
+        const repeatedText = Array(10)
+            .fill(`<span class="marquee-icon">✦</span> <span class="marquee-text">${text}</span>`)
+            .join(" ");
+
+        marqueeText.innerHTML = repeatedText;
+    }
+});
